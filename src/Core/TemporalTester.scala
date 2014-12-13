832d3b25c4f956a60ebe4e93115461d7a39657ff
ducky <ducky@localhost.localdomain>
2014-12-12 19:04:26 -0800

    Add ISA Test framework

--- src/Core/TemporalTester.scala
+++ src/Core/TemporalTester.scala
@@ -12,18 +12,35 @@ class TemporalTesterTimer[+T <: Module](val tester: TemporalTester[T]) {
   def expectEqualsAtAndAdvance(data: Bits, x: BigInt, deltaCycleMin: Int,
                                deltaCycleAdvance: Int, deltaCycleMax: Int,
                                betweenConstants: Map[Bits, BigInt]) {
-    
+    assert(false)  // TODO Implement me
   }
   
 }
 
-// TODO: should isTrace be passed?
-class TemporalTester[+T <: Module](c: T, val frequency:Int, isTrace: Boolean = false)
+class TemporalTester[+T <: Module](c: T, val frequency:Int,
+                                   val max_cycle: Int = 0,
+                                   isTrace: Boolean = false)
                                    extends Tester(c, isTrace) {
   var cycle: Int = 0
   override def step(n: Int) {
     cycle = cycle + n
+    if (cycle > max_cycle) {
+      println(s"TemporalTester: cycle count limit exceeded: $cycle > $max_cycle")
+      assert(false)  // TODO: better failure mode
+    }
     super.step(n)
+  }
+  
+  def stepUntilEqual(data: Bits, value: BigInt) {
+    while (peek(data) != value) {
+      step(1)
+    }
+  }
+  
+  def stepUntilNotEqual(data: Bits, value: BigInt) {
+    while (peek(data) == value) {
+      step(1)
+    }
   }
 }
 
